8cc07f73d07ebb5f9aef794a942576085b711e4f
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-12 20:43:48 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,6 @@
 def second_challenge
   epic_tragedy = {
-    :montague => { patriarch: {}, matriarch: {}, hero:{}, hero_friends:{}}, 
+    :montague => { patriarch: {}, matriarch: {}, hero:{}, hero_friends:{}},
     :capulet => { patriarch: {}, matriarch: {}, heroine:{}, heroine_friends:{}}
-      #your key/value pairs here!
       }}
-
-  
 end
